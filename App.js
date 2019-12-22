493e25c4a409f44097efb2db6f5645dd85b9dd49
ysurutani <>
2019-12-22 23:54:20 +0900

    text changed

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>やって参りました。React Native!</Text>
     </View>
   );
 }
